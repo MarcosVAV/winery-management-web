621dfa3b00a2fcc9a9636d153a38dc2efd37bbbd
Marcos Veloso <marcosvictor12345@gmail.com>
2022-10-22 20:19:41 -0300

    add axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from "./router"
 import store from "./store"
 import vuetify from "./plugins/vuetify"
 
+import axios from "axios"
+import VueAxios from "vue-axios"
+
+Vue.use(VueAxios, axios)
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/api"
+
 Vue.config.productionTip = false
 
 new Vue({
